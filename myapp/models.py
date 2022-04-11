a7388ce1f0aec335a771604c8a732ac10ddc0df1
Darnell Dugger <darnelldugger@gmail.com>
2022-04-11 13:29:41 -0400

    add dish post model, complete

--- myapp/models.py
+++ myapp/models.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('DishPost', backref='restaurant', lazy=True)
 
     def __init__(self, email, username, password):
         self.email = email
@@ -31,3 +32,22 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Username {self.username}"
 
+class DishPost(db.Model):
+    __tablename__ = 'dish_posts'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+    def __init__(self, title, price, description, user_id):
+        self.title = title
+        self.price = price
+        self.description = description
+        self.user_id = user_id
+
+
+    def __repr__(self):
+        return f"Post ID: {self.id} -- Date: {self.date} --- Title: {self.Title}"
