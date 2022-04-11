42c355db679febc0d5c8a6e40e4634337a23007a
Darnell Dugger <darnelldugger@gmail.com>
2022-04-11 19:07:09 -0400

    add update function to dish post views

--- myapp/dish_posts/views.py
+++ myapp/dish_posts/views.py
@@ -23,3 +23,28 @@ def create_post():
 def dish_post(dish_post_id):
     dish_post = DishPost.query.get_or_404(dish_post_id) 
     return render_template('dish_post.html', title=dish_post.title, date=dish_post.date, post=dish_post)
+
+@dish_posts.route('/<int:dish_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(dish_post_id):
+    dish_post = DishPost.query.get_or_404(dish_post_id)
+
+    if dish_post.author != current_user:
+        abort(403)
+
+    form = DishPostForm()
+
+    if form.validate_on_submit():
+        dish_post.title = form.title.data
+        dish_post.price = form.price.data
+        dish_post.description = form.description.data
+        db.session.commit()
+        flash('Dish Post Updated')
+        return redirect(url_for('dish_posts.dish_post',dish_post_id=dish_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = dish_post.title
+        dish_post.price = form.price.data
+        dish_post.description = form.description.data
+
+    return render_template('create_post.html',title='Updating',form=form)
